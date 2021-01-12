62d062cd165d4032931619764660d9f343e0c350
sachin gupta <sachin2320@gmail.com>
2021-01-12 13:35:05 +0530

    commit

---
+++ Test-VS2015-CiCd/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Test_VS2015_CiCd
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Test-VS2015-CiCd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test_VS2015_CiCd.Startup))]
+namespace Test_VS2015_CiCd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
